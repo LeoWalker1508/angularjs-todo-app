12f9dc3673e68bbe70353bd28af611be4de11698
LeoWalker1508 <leowalkerpokka@gmail.com>
2019-03-06 19:19:05 +0100

    Added remove function

--- todo.js
+++ todo.js
@@ -13,9 +13,16 @@ angular.module('todoApp', [])
       $scope.todoText = '';
     }
     $scope.add = add;
+
+    function remove(todoRemove) {
+      console.log(todoRemove)
+      var provisionalTodoArray = [];
+      for (var index = 0; index < $scope.todos.length; index++) {
+        if ($scope.todos[index].name !== todoRemove) {
+          provisionalTodoArray.push($scope.todos[index]);
+        }
+      }
+      $scope.todos = provisionalTodoArray;
+    }
+    $scope.remove = remove;
   })
-/*
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-*/
