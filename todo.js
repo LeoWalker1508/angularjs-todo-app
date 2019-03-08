ab8fbe6fb8bf2f0301fb6984ebad5c3a6ff70ff1
LeoWalker1508 <leowalkerpokka@gmail.com>
2019-03-08 15:45:02 +0100

    Added delete function with uuid

--- todo.js
+++ todo.js
@@ -3,6 +3,18 @@ angular.module('todoApp', [])
     $scope.todos =[];
     $scope.todoText = '';
     $scope.archived = [];
+//Commit this tomorrow at the train
+    function generateUUID() {
+      var d = new Date().getTime();
+      if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+          d += performance.now(); //use high-precision timer if available
+      }
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+          var r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+  }
 
     function add () {
       if ($scope.todoText.length === 0 || $scope.todoText === ' ') {
@@ -10,27 +22,25 @@ angular.module('todoApp', [])
       } else {
         $scope.todos.push({
           name: $scope.todoText,
-          done: false
+          done: false,
+          uuid: generateUUID()
        });
         $scope.todoText = '';
+        console.log( $scope.todos)
       }
     }
 
     $scope.add = add;
 
-    function remove(todoRemove) {
-      var removeText = 'Do you want to remove ' + todoRemove + '?';
+    function remove(removeObject) {
+      var removeText = 'Do you want to remove ' + removeObject.name + '?';
       var confirmation = window.confirm(removeText);
       if (confirmation) {
-      var provisionalTodoArray = [];
-      for (var index = 0; index < $scope.todos.length; index++) {
-        if ($scope.todos[index].name !== todoRemove) {
-          provisionalTodoArray.push($scope.todos[index]);
-        }
-      }
-      $scope.todos = provisionalTodoArray;
+      $scope.todos = $scope.todos.filter(function(val) {
+        val.uuid = removeObject.uuid;
+      });
     }
-   }
+  }
 
     $scope.remove = remove;
 
